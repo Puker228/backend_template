c9fe8886f59ec37052a8b1fbace1e80899c6847e
danil <karatelanalov@yandex.ru>
2025-03-19 22:36:38 +0300

    add docker

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from config import settings
@@ -11,3 +12,12 @@ app = FastAPI(
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
